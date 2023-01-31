8af0434ccaa3beb9cd56818ab90bbfef2dba1f65
Andrew71 <77640396+Andrew-71@users.noreply.github.com>
2023-01-31 10:27:52 +0300

    0.7

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"su.a71.tardim_ic.tardim_ic"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"su.a71.tardim_ic.tardim_ic"},{"l":"su.a71.tardim_ic.tardim_ic.command"}];updateSearchResults();
